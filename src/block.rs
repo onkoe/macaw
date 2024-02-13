5b2fa8407753e81fd7ca5651a1b616df483f8cfe
barrett <onkoe@protonmail.com>
2024-02-13 02:50:53 -0600

    feat: Add `is_transparent()` to `Block`

--- src/block.rs
+++ src/block.rs
@@ -10,12 +10,21 @@ impl Block {
     pub fn new(block_type: BlockType, state: u32) -> Self {
         Self { block_type, state }
     }
+
+    /// If a block type is transparent, like air, liquids, or glass, this
+    /// method will let you know.
+    pub fn is_transparent(&self) -> bool {
+        // if it's one of these, then yes.
+        // otherwise, no.
+        matches!(self.block_type, BlockType::Air | BlockType::Water)
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, PartialOrd, Ord, Eq, Hash)]
 #[allow(unused)]
 pub enum BlockType {
     Air,
+    Water,
     Stone,
     Log,
     Dirt,
