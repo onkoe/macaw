5f022f5092158e64831c65f4772fbf9ae1f77fcf
barrett <onkoe@protonmail.com>
2024-02-17 19:18:03 -0600

    feat: Remove old meshing methods from `Block`

--- src/renderer.rs
+++ src/renderer.rs
@@ -14,13 +14,12 @@ impl Plugin for MacawRendererPlugin {
 }
 
 impl MacawRendererPlugin {
-    #[allow(unused)]
     fn setup(
         mut commands: Commands,
         mut meshes: ResMut<Assets<Mesh>>,
         mut materials: ResMut<Assets<StandardMaterial>>,
         mut window_query: Query<&mut Window, With<PrimaryWindow>>,
-        assets: Res<AssetServer>,
+        asset_server: Res<AssetServer>,
     ) {
         // enable mouse lock
         let mut window = window_query.single_mut();
@@ -28,54 +27,27 @@ impl MacawRendererPlugin {
 
         let world = crate::world::World::generate_test_chunk();
 
-        fn handle_builder(assets: &Res<AssetServer>, path: &str) -> Handle<Image> {
-            assets.load(path.to_owned())
-        }
+        let mesh = Cuboid::new(1.0, 1.0, 1.0).mesh();
 
-        fn material_builder(
-            materials: &mut ResMut<'_, Assets<StandardMaterial>>,
-            image_handle: Handle<Image>,
-        ) -> Handle<StandardMaterial> {
-            materials.add(StandardMaterial {
-                base_color: Color::RED,
-                base_color_texture: Some(image_handle),
-                reflectance: 1.0,
-                metallic: 0.0,
+        commands.spawn(PbrBundle {
+            mesh: meshes.add(mesh),
+            transform: Transform {
+                translation: Vec3::new(100.0, 0.0, 0.0),
                 ..Default::default()
-            })
-        }
+            },
+            // material: `get_texture_from_block_type(block_type)`,
+            material: materials.add(StandardMaterial {
+                base_color: Color::RED,
+                base_color_texture: Some(
+                    asset_server.load("/home/barrett/Documents/macaw/assets/stone.png"),
+                ),
+                reflectance: 1.0,
+                metallic: 0.1,
+                ..Default::default()
+            }),
+            ..Default::default()
+        });
 
-        let stone_handle: Handle<Image> =
-            handle_builder(&assets, "/home/barrett/Documents/macaw/assets/stone.png");
-        let grass_handle: Handle<Image> =
-            handle_builder(&assets, "/home/barrett/Documents/macaw/assets/grass.png");
-        let dirt_handle: Handle<Image> =
-            handle_builder(&assets, "/home/barrett/Documents/macaw/assets/dirt.png");
-
-        let _stone_material = material_builder(&mut materials, stone_handle);
-        let _dirt_material = material_builder(&mut materials, dirt_handle);
-        let _grass_material = material_builder(&mut materials, grass_handle);
-
-        for (chunk_location, chunk) in world.chunks().clone() {
-            //tracing::debug!("chunk: `{chunk_location:?}`");
-
-            let calculated_meshes = meshing::greedy::meshing(&chunk);
-
-            for (transform, mesh) in calculated_meshes {
-                commands.spawn(PbrBundle {
-                    mesh: meshes.add(mesh),
-                    transform,
-                    //material: stone_material.clone(),
-                    /*match b.block_type {
-                        block::BlockType::Grass => grass_material.clone(),
-                        block::BlockType::Dirt => dirt_material.clone(),
-                        _ => stone_material.clone(),
-                    },*/
-                    ..Default::default()
-                });
-            }
-        }
-
-        tracing::info!("done 'rendering' world");
+        meshing_two::render_clusters(&mut commands, meshes, world, asset_server, materials);
     }
 }
