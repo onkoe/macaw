f625c4205602ebd16d2530a7c29e0ec01d82c30c
Barrett <onkoe@protonmail.com>
2024-04-06 15:17:22 -0500

    feat: Add save constants

--- shared/src/world/save.rs
+++ shared/src/world/save.rs
@@ -7,12 +7,19 @@
 // and i'll leave the option open for anyone who wants to give it
 // a try!
 
-pub enum WorldType {
-    Generated(Generator),
-    Empty,
-}
+use std::path::PathBuf;
 
-pub enum Generator {
-    Default,
-    Custom(Fn),
+pub const GAME_DIRECTORY: &str = "macaw";
+pub const SAVES_DIRECTORY: &str = "saves";
+
+/// Gets the path where all game saves are kept. This is currently
+/// 'hard-coded', but should later take user configuration during launch.
+pub fn get_saves_path() -> Result<PathBuf, ()> {
+    let all_dirs = directories::ProjectDirs::from("", "", GAME_DIRECTORY).ok_or(())?;
+    let dir = all_dirs.config_dir();
+
+    let mut path = PathBuf::new();
+    path.push(dir);
+    path.push(SAVES_DIRECTORY);
+    Ok(path)
 }
